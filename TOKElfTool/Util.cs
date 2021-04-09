f1cac4e580dfab146755c1e02940f4af5d02bd5a
Darxoon <darxoon@outlook.com>
2021-04-09 16:08:02 +0200

    Add shortening of displayed paths

--- TOKElfTool/Util.cs
+++ TOKElfTool/Util.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Markup;
@@ -32,5 +33,33 @@ namespace TOKElfTool
 
         public static T Last<T>(this IList<T> source) => source[source.Count - 1];
         public static object Last(this IList source) => source[source.Count - 1];
+
+        private static readonly Regex shortenPathRegex = new Regex(@"[\/\\]");
+        private const int MAX_PATH_SEGMENT_AMOUNT = 6;
+
+        public static string ShortenPath(string path)
+        {
+            string[] segments = shortenPathRegex.Split(path);
+            int length = 0;
+            bool ending = false;
+
+            // reverse loop
+            for (int i = segments.Length - 1; i >= 0; i--)
+            {
+                length++;
+
+                if (ending)
+                    break;
+
+                if (segments[i] == "romfs" || segments.Length - i >= MAX_PATH_SEGMENT_AMOUNT - 1)
+                    ending = true;
+
+            }
+
+            string[] result = new string[length];
+            Array.Copy(segments, segments.Length - length, result, 0, length);
+
+            return result.Length < segments.Length ? $@"...\{string.Join(@"\", result)}" : string.Join(@"\", result);
+        }
     }
 }
